df26dddb8faf440e891ec7697828eb898045177c
lilmoussis <daoudassa@gmail.com>
2022-05-31 20:49:18 +0000

    ...

--- resources/views/admins/login.blade.php
+++ resources/views/admins/login.blade.php
@@ -1,8 +1,8 @@
 @extends('base')
 
 @section('login')
-    {{-- {{@bcrypt('admin')}} --}}
-    <h1>Login Admin</h1>
+    {{-- {{@bcrypt('admin')}}
+    
     @if($message=Session::get('error'))
         <div class="">{{$message}}</div>
     @endif
@@ -13,5 +13,53 @@
         <button type="submit">
             Se connecter
         </button>
-    </form>
+    </form> --}}
+
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+									<div class="text-center mb-3">
+										<img src="images/logo-full.png" alt="">
+									</div>
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                    <form action="{{ route('admins.loginform')}}">
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Utilisateur</strong></label>
+                                            <input type="username" class="form-control" value=">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input type="password" class="form-control" value="Password">
+                                        </div>
+                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+                                            <div class="form-group">
+                                               <div class="custom-control custom-checkbox ml-1">
+													<input type="checkbox" class="custom-control-input" id="basic_checkbox_1">
+													<label class="custom-control-label" for="basic_checkbox_1">Remember my preference</label>
+												</div>
+                                            </div>
+                                            <div class="form-group">
+                                                <a href="page-forgot-password.html">Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+                                        </div>
+                                    </form>
+                                    <div class="new-account mt-3">
+                                        <p>Don't have an account? <a class="text-primary" href="page-register.html">Sign up</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,74 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Immobilier</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="{{URL::asset('assets/images/favicon.png')}}">
+	<link rel="stylesheet" href="{{URL::asset('assets/vendor/chartist/css/chartist.min.css')}}">
+    <link href="{{URL::asset('assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
+	<link href="{{URL::asset('assets/vendor/owl-carousel/owl.carousel.css')}}" rel="stylesheet">
+    <link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-  rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.css"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.js"
-></script>
-
-<style>
-    body {
-        background-color: #fbfbfb;
-    }
-    @media (min-width: 991.98px) {
-        main {
-            padding-left: 240px;
-        }
-    }
-
-    /* Sidebar */
-    .sidebar {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        padding: 58px 0 0; /* Height of navbar */
-        box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
-        width: 240px;
-        z-index: 600;
-    }
-
-    @media (max-width: 991.98px) {
-        .sidebar {
-            width: 100%;
-        }
-    }
-    .sidebar .active {
-        border-radius: 5px;
-        box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
-    }
-
-    .sidebar-sticky {
-        position: relative;
-        top: 0;
-        height: calc(100vh - 48px);
-        padding-top: 0.5rem;
-        overflow-x: hidden;
-        overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-    }
-</style>
-
 </head>
 <body>
     @yield('login')
     @yield('content')
+
+    <script data-cfasync="false" src="../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="{{URL::asset('assets/vendor/global/global.min.js')}}"></script>
+	<script src="{{URL::asset('assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
+	<script src="{{URL::asset('assets/vendor/chart.js/Chart.bundle.min.js')}}"></script>
+	
+	<!-- Chart piety plugin files -->
+    <script src="{{URL::asset('assets/vendor/peity/jquery.peity.min.js')}}"></script>
+	
+	<!-- Apex Chart -->
+	<script src="{{URL::asset('assets/vendor/apexchart/apexchart.js')}}"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="{{URL::asset('assets/js/dashboard/dashboard-1.js')}}"></script>
+	
+	<script src="{{URL::asset('assets/vendor/owl-carousel/owl.carousel.js')}}"></script>
+    <script src="{{URL::asset('assets/js/custom.min.js')}}"></script>
+	<script src="js/deznav-init.js"></script>
+    <script src="{{URL::asset('assets/js/demo.js')}}"></script>
+    <script src="{{URL::asset('assets/js/styleSwitcher.js')}}"></script>
 </body>
 </html>
